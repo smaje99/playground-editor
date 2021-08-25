9a6e1668fef8dda2858d05474a48a9d0acb6bb68
Sergio Majé <smajefranco@gmail.com>
2021-08-24 21:02:37 -0500

    Create HTML
    * Text area events

--- main.js
+++ main.js
@@ -6,14 +6,33 @@ const $js = $('#js');
 const $css = $('#css');
 const $html = $('#html');
 
-$js.addEventListener('input', e => {
-
-});
-
-$css.addEventListener('input', e => {
+const createHTML = () => {
+  const html = $html.value;
+  const css = $css.value;
+  const js = $js.value;
   
-});
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <style>
+  ${css}
+  </style>
+  </head>
+  <body>
+  ${html}
+  <script>
+  ${js}
+  </script>
+  </body>
+  </html>`
+}
 
-$html.addEventListener('input', e => {
-  
-});
+const update = () => {
+  const html = createHTML();
+  $('iframe').setAttribute('srcdoc', html);
+}
+
+$js.addEventListener('input', update);
+$css.addEventListener('input', update);
+$html.addEventListener('input', update);
