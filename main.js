eb5dc242208d38302894f8e5ccf8640f15e9ba87
Sergio Majé <smajefranco@gmail.com>
2021-08-24 20:39:17 -0500

    Estructura del Editor

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import './style.css'
 
+const $ = selector => document.querySelector(selector);
 
+const $js = $('#js');
+const $css = $('#css');
+const $html = $('#html');
+
+$js.addEventListener('input', e => {
+
+});
+
+$css.addEventListener('input', e => {
+  
+});
+
+$html.addEventListener('input', e => {
+  
+});
