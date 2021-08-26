400daead8c42e7e91a8ae4ed2693a6b4b55ccc94
Sergio Majé <smajefranco@gmail.com>
2021-08-25 22:32:14 -0500

    Split editors in playground

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+import Split from "split-grid";
+
 const $ = selector => document.querySelector(selector);
+
+Split({
+  columnGutters: [{
+    track: 1,
+    element: $('.vertical-gutter')
+  }],
+  rowGutters: [{
+    track: 1,
+    element: $('.horizontal-gutter')
+  }]
+})
 
 const $js = $('#js');
 const $css = $('#css');
