9920338a6668ae28131e4ec8faf84daf3ebcc1fe
Sergio Majé <smajefranco@gmail.com>
2021-08-27 19:52:02 -0500

    remove Split from main.js

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-import Split from "split-grid";
-
 const $ = selector => document.querySelector(selector);
-
-Split({
-  columnGutters: [{
-    track: 1,
-    element: $('.vertical-gutter')
-  }],
-  rowGutters: [{
-    track: 1,
-    element: $('.horizontal-gutter')
-  }]
-})
 
 const $js = $('#js');
 const $css = $('#css');
