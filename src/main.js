f0eeacbb03f2ca9a23559e677606e92ebc7b62f0
Sergio Majé <smajefranco@gmail.com>
2021-09-12 15:30:04 -0500

    Integration of monaco editors in the playground

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { encode, decode } from 'js-base64';
 
 import { $ } from './utils/dom.js';
+import { createEditor } from './editor.js'
 
 import './split.js';
 
@@ -8,55 +9,61 @@ const $js = $('#js');
 const $css = $('#css');
 const $html = $('#html');
 
-const createHTML = ({ html, js, css }) => {
+const { pathname } = window.location;
+
+const [html, js, css] = pathname.slice(1)
+                                .split(encodeURI('|'))
+                                .map(value => value ? decode(value) : '');
+
+const htmlEditor = createEditor({ domElement: $html, language: 'html', value: html });
+const cssEditor = createEditor({ domElement: $css, language: 'css', value: css });
+const jsEditor = createEditor({ domElement: $js, language: 'javascript', value: js });
+
+htmlEditor.focus();
+htmlEditor.onDidChangeModelContent(update);
+cssEditor.onDidChangeModelContent(update);
+jsEditor.onDidChangeModelContent(update);
+
+updateValues( { htmlValue: html, jsValue: js, cssValue: css })
+
+function createHTML({ htmlValue, jsValue, cssValue }) {
   return `
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <style>
-    ${css}
+    ${cssValue}
   </style>
 </head>
 <body>
-  ${html}
+  ${htmlValue}
   <script>
-    ${js}
+    ${jsValue}
   </script>
 </body>
 </html>`
 }
 
-const update = () => {
-  const html = $html.value;
-  const js = $js.value;
-  const css = $css.value;
+function update() {
+  const htmlValue = htmlEditor.getValue();
+  const jsValue = jsEditor.getValue();
+  const cssValue = cssEditor.getValue();
 
+  updateURL({ htmlValue, jsValue, cssValue });
+  updateValues({ htmlValue, jsValue, cssValue });
+}
+
+function updateURL({ htmlValue, jsValue, cssValue }) {
   // Base64 encryption of editors content
-  const hashedCode = `${encode(html)}|${encode(js)}|${encode(css)}`;
+  const hashedCode = `${encode(htmlValue)}|${encode(jsValue)}|${encode(cssValue)}`;
 
   // URL modification
   window.history.replaceState(null, null, `/${hashedCode}`);
-
-  const htmlForPreview = createHTML({ html, js, css });
-  $('.playground').setAttribute('srcdoc', htmlForPreview);
 }
 
-const init = () => {
-  const { pathname } = window.location;
-
-  const [html, js, css] = pathname.slice(1)
-                                  .split(encodeURI('|'))
-                                  .map(content => decode(content));
-  $html.value = html;
-  $js.value = js;
-  $css.value = css;
-
-  const htmlForPreview = createHTML({ html, js, css });
-  $('.playground').setAttribute('srcdoc', htmlForPreview);
+function updateValues( { htmlValue, jsValue, cssValue }) {
+  $('.playground').setAttribute(
+    'srcdoc',
+    createHTML({ htmlValue, jsValue, cssValue })
+  );
 }
-
-$js.addEventListener('input', update);
-$css.addEventListener('input', update);
-$html.addEventListener('input', update);
-
-init();
